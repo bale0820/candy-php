92ffb7e3df6bb67b98fafc37ebcc9f525283420a
bale <dbsgywls667@gmail.com>
2026-06-30 19:07:58 +0900

    fix: 처음 생성

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['https://vue-frontend-seven.vercel.app'],
 
     'allowed_origins' => ['*'],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
